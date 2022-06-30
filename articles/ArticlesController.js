d5d45a0efe5dc684a76cf04342b538a98df6b2e6
luizmenezesss <106546683+luizmenezesss@users.noreply.github.com>
2022-06-30 17:51:22 -0300

    primeiro commit

---
+++ articles/ArticlesController.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const router = express.Router();
+const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
+
+router.get("/admin/articles", (req, res) => {
+    Article.findAll({
+        include: [{ model: Category }]
+    }).then(articles => {
+        res.render("admin/articles/index", { articles: articles })
+    });
+
+});
+
+router.get("/admin/articles/new", (req, res) => {
+    Category.findAll().then(categories => {
+        res.render("admin/articles/new", { categories: categories })
+    })
+})
+
+router.post("/articles/save", (req, res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(() => {
+        res.redirect("/admin/articles")
+    });
+
+
+});
+
+router.post("/articles/delete", (req, res) => {
+    var id = req.body.id;
+    if (id != undefined) {
+        if (!isNaN(id)) {
+
+            Article.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/articles");
+            });
+        } else {// Não for um Número
+            res.redirect("/admin/articles");
+        }
+    } else {// NULL
+        res.redirect("/admin/articles");
+    }
+});
+
+router.get("/admin/articles/edit/:id", (req, res) => {
+    var id = req.params.id;
+
+    if (isNaN(id)) {
+        res.redirect("/admin/articles");
+    }
+
+    Article.findByPk(id).then(article => {
+        console.log(article);
+        if (article != undefined) {
+
+            Category.findAll().then(categories => {
+                res.render("admin/articles/edit", {
+                    categories: categories,
+                    article: article,
+                })
+            });
+
+        } else {
+            res.redirect("/");
+        }
+    }).catch(err => {
+        res.redirect("/");
+
+    });
+
+});
+module.exports = router;
